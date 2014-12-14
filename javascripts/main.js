f5a35b43351bbb8d4456ef35274d4f6ee5f01eec
Josh Shemas <jshemas@hotmail.com>
2014-12-14 14:20:04 -0500

    add directive to display time

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,3 +3,16 @@ var colorTimeApp = angular.module('colorTimeApp', []);
 colorTimeApp.controller('colorTimeCtrl', function ($scope) {
 	$scope.welecomeMessage = 'Welcome';
 });
+
+colorTimeApp.directive("displayCurrentTime", function(dateFilter) {
+	return function(scope, element, attrs) {
+		element.text(dateFilter(new Date(), 'h:mm:ss a'));
+		function updateInOneSecond() {
+			setTimeout(function() {
+				element.text(dateFilter(new Date(), 'h:mm:ss a'));
+				updateInOneSecond();
+			}, 1000);
+		}
+		updateInOneSecond();
+	}
+});
